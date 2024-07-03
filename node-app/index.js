fbc0be5940fde0243cc86ded0c3c65e93210dd7a
Niranjan A S <niranjan0881@gmail.com>
2024-07-03 20:46:06 +0530

    chore: sample node kub app

---
+++ node-app/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (_req, res) {
+    return res.send(`
+            <h1>NodeJS Application</h1>
+            <p>Try sending request to /error</p>
+        `);
+});
+
+app.get('/error', function () {
+    process.exit(1);
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, function () {
+    console.log(`Server running on ${PORT}`);
+});
