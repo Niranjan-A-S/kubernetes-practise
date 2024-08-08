4c1317ca18b8ab8280f3baa76c5c6c89a6db1bf6
Niranjan A S <niranjan0881@gmail.com>
2024-08-08 17:11:51 +0530

    chore: exposed ports

--- kube-net/users-service/app.js
+++ kube-net/users-service/app.js
@@ -22,9 +22,8 @@ app.post('/signup', async (req, res) => {
     }
 
     try {
-        // const hashedPW = await axios.get('http://auth/hashed-password/' + password);
-        const response = await axios.get(`${process.env.AUTH_ADDRESS}/hashed-password/` + password);
-        // const hashedPW = 'dummy text';
+        // const response = await axios.get(`${process.env.AUTH_ADDRESS}/hashed-password/` + password);
+        const hashedPW = 'dummy text';
         // since it's a dummy service, we don't really care for the hashed-pw either
         console.log(response.data?.hashedPassword, email);
         res.status(201).json({ message: 'User created!' });
@@ -54,15 +53,10 @@ app.post('/login', async (req, res) => {
 
     // normally, we'd find a user by email and grab his/ her ID and hashed password
     const hashedPassword = password + '_hash';
-    // for docker-compose
     // const response = await axios.get(
-    //     `http://auth/token/` + hashedPassword + '/' + password
+    //     `${process.env.AUTH_ADDRESS}/token/${hashedPassword}/${password}`
     // );
-    // for kubernetes
-    const response = await axios.get(
-        `${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
-    );
-    // const response = { status: 200, data: { token: 'abc' }};
+    const response = { status: 200, data: { token: 'abc' } };
     if (response.status === 200) {
         return res.status(200).json({ token: response.data.token });
     }
